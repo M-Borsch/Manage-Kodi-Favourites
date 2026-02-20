58d84ff9ed29caf53da4531d3ad6de31be2ea8d8
Mike Borsch <154018836+M-Borsch@users.noreply.github.com>
2026-02-20 18:57:12 -0500

    Update default.py

--- default.py
+++ default.py
@@ -102,7 +102,7 @@ class CustomFavouritesDialog(xbmcgui.WindowXMLDialog):
             li.setArt(artDict)
 
             # TEST - Try modifying prefix for all
-            li.setLabel("##Mike -" + "TEST")
+            li.setLabel("##Mike -" + data[0])
             
             li.setProperty('index', str(index)) # To help with resetting, if necessary.
             yield li
@@ -534,6 +534,7 @@ else:
         )
     )
     xbmcplugin.endOfDirectory(PLUGIN_ID)
+
 
 
 
