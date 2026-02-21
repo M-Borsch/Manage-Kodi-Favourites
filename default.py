685bcf16c550e05853e330e4e40523e3aa2c5210
Mike Borsch <154018836+M-Borsch@users.noreply.github.com>
2026-02-21 15:19:21 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- default.py
+++ default.py
@@ -318,16 +318,23 @@ class CustomFavouritesDialog(xbmcgui.WindowXMLDialog):
 
             if DEBUG == '1': xbmcgui.Dialog().ok('Manage Kodi Favourites', 'INFO: "%s"\n(New Label)' %  str(new_label))
 
+            # Let the user know that there are about to modify a List entry
+            msg_text = f"This will modify the Prefix/Suffix/Color of the currently selected item to:.\n{new_label}\nProceed?"
+            if xbmcgui.Dialog().yesno(
+                    'Manage Kodi Favourites',
+                    msg_text
+            ):         
+                
             # Check setting to see if we should let the user know that there are about to modify a List entry
-            verbose = False if not ADDON.getSetting('presuffixBool') else ADDON.getSetting('presuffixBool')
-            if verbose == True:
-                if DEBUG == '1': xbmcgui.Dialog().ok('Manage Kodi Favourites', 'INFO: "%s"\n(thumSize is SMALL)' % thumbSize)
-                ui = CustomFavouritesDialog('CustomFavouritesDialog-smThumbs.xml', ADDON.getAddonInfo('path'), 'Default', '1080i')
-                msg_text = f"This will modify the Prefix/Suffix/Color of the currently selected item to:.\n{new_label}\nProceed?"
-                if xbmcgui.Dialog().yesno(
-                        'Manage Kodi Favourites',
-                        msg_text
-                ):
+            # verbose = False if not ADDON.getSetting('presuffixBool') else ADDON.getSetting('presuffixBool')
+            # if verbose == True:
+            #    if DEBUG == '1': xbmcgui.Dialog().ok('Manage Kodi Favourites', 'INFO: "%s"\n(thumSize is SMALL)' % thumbSize)
+            #    ui = CustomFavouritesDialog('CustomFavouritesDialog-smThumbs.xml', ADDON.getAddonInfo('path'), 'Default', '1080i')
+            #    msg_text = f"This will modify the Prefix/Suffix/Color of the currently selected item to:.\n{new_label}\nProceed?"
+            #    if xbmcgui.Dialog().yesno(
+            #            'Manage Kodi Favourites',
+            #            msg_text
+            #    ):
                         
             # Show the change to the list item
             listitem_at_index.setLabel(new_label)
@@ -564,6 +571,7 @@ else:
         )
     )
     xbmcplugin.endOfDirectory(PLUGIN_ID)
+
 
 
 
