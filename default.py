907844dd2d3c506d99cb0dd1a63cc51c84e8aba5
Mike Borsch <154018836+M-Borsch@users.noreply.github.com>
2026-03-04 15:01:19 -0500

    Update default.py

--- default.py
+++ default.py
@@ -781,10 +781,10 @@ else:
     configureItem = xbmcgui.ListItem('[B]Configure... (Change Settings)[/B]')
     configureItem.setArt({'thumb': 'DefaultFolderBack.png'})
     configureItem.setInfo('video', {'plot': 'Configure the default actions in Settings panel for Prefix, Suffix, Colors and Insert/Swap modes.'})
-    writeoutFavs = xbmcgui.ListItem('[COLOR red][B]Write Out Favourites [/COLOR](Advanced! - Save a Copy of Favourites file[/B]')
+    writeoutFavs = xbmcgui.ListItem('[COLOR red][B]-> Write Out Favourites [/COLOR](Advanced! - Save a Copy of Favourites file[/B]')
     writeoutFavs.setArt({'thumb': 'DefaultFolderBack.png'})
     writeoutFavs.setInfo('video', {'plot': 'Advanced - Write out a copy of your Kodi Favourites file.[/COLOR]'})
-    overwriteFavs = xbmcgui.ListItem('[COLOR red][B]Overwrite Favourites [/COLOR](Advanced! - Overwrite Favourites file - Leave Changes Pending a Kodi Restart or [COLOR orange]Reload Profile[/COLOR])[/B]')
+    overwriteFavs = xbmcgui.ListItem('[COLOR red][B]-> Overwrite Favourites [/COLOR](Advanced! - Overwrite Favourites file - Leave Changes Pending a Kodi Restart or [COLOR orange]Reload Profile[/COLOR])[/B]')
     overwriteFavs.setArt({'thumb': 'DefaultFolderBack.png'})
     overwriteFavs.setInfo('video', {'plot': 'Advanced - Overwrite Kodi Favourites file, Leave Changes Pending a Kodi Restart or [COLOR orange]Reload Profile.[/COLOR]'})
     exitItem = xbmcgui.ListItem('[B]Exit (No Save-Exit - Abandon Any Unsaved Changes)[/B]')
@@ -807,6 +807,7 @@ else:
         )
     )
     xbmcplugin.endOfDirectory(PLUGIN_ID)
+
 
 
 
